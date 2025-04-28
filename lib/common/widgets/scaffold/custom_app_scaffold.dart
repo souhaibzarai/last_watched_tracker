7252bd7f53794a684bcbc1fdf1d957dd5e0b3644
souhaibzarai <souhaibzarai18@gmail.com>
2025-04-28 23:51:37 +0100

    custom scaffold

--- lib/common/widgets/scaffold/custom_app_scaffold.dart
+++ lib/common/widgets/scaffold/custom_app_scaffold.dart
@@ -3,38 +3,17 @@ import 'package:flutter/material.dart';
 import '../../../utils/theme/app_colors.dart';
 
 class CustomAppScaffold extends StatelessWidget {
-  const CustomAppScaffold({
-    super.key,
-    required this.child,
-    required this.header,
-  });
+  const CustomAppScaffold({super.key, required this.child});
   final Widget child;
-  final Widget header;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          color: AppColors.disabledColor,
-          child: Column(
-            children: [
-              header,
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-                  decoration: BoxDecoration(
-                    color: AppColors.primaryColor,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20), //
-                      topRight: Radius.circular(20), //
-                    ),
-                  ),
-                  child: child,
-                ),
-              ),
-            ],
-          ),
+          decoration: BoxDecoration(color: AppColors.primaryColor),
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+          child: child,
         ),
       ),
     );
