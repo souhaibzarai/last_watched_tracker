439c6ab7bdbbb8661520b45bbb7ac10624646d8d
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-01 17:39:50 +0100

    media repository

---
+++ lib/features/media/data/repository/media_repo_impl.dart
@@ -0,0 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:last_watched_tracker/features/media/data/models/media.dart';
+import 'package:last_watched_tracker/features/media/data/source/media_firebase_source.dart';
+import 'package:last_watched_tracker/features/media/domain/entities/media.dart';
+import 'package:last_watched_tracker/features/media/domain/repository/media_repo.dart';
+import 'package:last_watched_tracker/service_locator.dart';
+
+class MediaRepoImpl implements MediaRepository {
+  @override
+  Future<Either> addNewMedia(MediaEntity media) async {
+    return await serviceLocator<MediaFirebaseSource>().addNewMedia(media);
+  }
+
+  @override
+  Future<Either> fetchAllMedia() async {
+    try {
+      final result =
+          await serviceLocator<MediaFirebaseSource>().fetchAllMedia();
+
+      return result.fold((err) => Left(err.toString()), (mediaList) {
+        final entities =
+            List.from(mediaList).map((media) {
+              final String id = media['id'];
+              final Map<String, dynamic> data = media['data'];
+              return MediaModel.fromFirestore(data, id: id).toEntity();
+            }).toList();
+
+        return Right(entities);
+      });
+    } catch (e) {
+      return Left('$e');
+    }
+  }
+}
---
+++ lib/features/media/domain/repository/media_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:last_watched_tracker/features/media/domain/entities/media.dart';
+
+abstract class MediaRepository {
+  Future<Either> addNewMedia(MediaEntity media);
+
+  Future<Either> fetchAllMedia();
+}
