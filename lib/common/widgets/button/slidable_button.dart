331c1dc84c35d17fb286ddeb4129865a34055d38
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-01 17:40:31 +0100

    custom slidable button

---
+++ lib/common/widgets/button/slidable_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class SlidableButton extends StatelessWidget {
+  const SlidableButton({
+    super.key,
+    this.color,
+    required this.icon,
+    required this.onClick,
+    this.label,
+    required this.bgColor,
+  });
+
+  final IconData icon;
+  final Color bgColor;
+  final Color? color;
+  final SlidableActionCallback onClick;
+  final String? label;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlidableAction(
+      spacing: 4,
+      onPressed: onClick,
+      backgroundColor: bgColor,
+      foregroundColor: color,
+      autoClose: true,
+      label: label,
+      icon: icon,
+    );
+  }
+}
