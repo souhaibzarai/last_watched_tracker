705ad63957b29484acb50f7c97c729a7aa704570
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-01 17:38:35 +0100

    media dependencies

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,13 +1,18 @@
 import 'package:get_it/get_it.dart';
-import 'package:last_watched_tracker/features/auth/data/repository/auth_repo_impl.dart';
-import 'package:last_watched_tracker/features/auth/data/source/firebase_auth_source.dart';
-import 'package:last_watched_tracker/features/auth/domain/usecases/get_user.dart';
-import 'package:last_watched_tracker/features/auth/domain/usecases/reset_password.dart';
-import 'package:last_watched_tracker/features/auth/domain/usecases/sign_in.dart';
-import 'package:last_watched_tracker/features/auth/domain/usecases/sign_up.dart';
-import 'package:last_watched_tracker/features/auth/domain/usecases/user_status.dart';
+import 'features/auth/data/repository/auth_repo_impl.dart';
+import 'features/auth/data/source/firebase_auth_source.dart';
+import 'features/auth/domain/usecases/get_user.dart';
+import 'features/auth/domain/usecases/reset_password.dart';
+import 'features/auth/domain/usecases/sign_in.dart';
+import 'features/auth/domain/usecases/sign_up.dart';
+import 'features/auth/domain/usecases/user_status.dart';
+import 'features/media/data/source/media_firebase_source.dart';
+import 'features/media/domain/repository/media_repo.dart';
+import 'features/media/domain/usecases/fetch_medias.dart';
+import 'features/media/domain/usecases/new_media.dart';
 
 import 'features/auth/domain/repository/auth_repository.dart';
+import 'features/media/data/repository/media_repo_impl.dart';
 
 final serviceLocator = GetIt.instance;
 
@@ -17,12 +22,18 @@ Future<void> initDependencies() async {
     AuthFirebaseServiceImpl(),
   );
 
+  serviceLocator.registerSingleton<MediaFirebaseSource>(
+    MediaFirebaseSourceImpl(),
+  );
+
   //
   //
   //
 
   // Repositories
   serviceLocator.registerSingleton<AuthRepository>(AuthRepositoryImpl());
+
+  serviceLocator.registerSingleton<MediaRepository>(MediaRepoImpl());
 
   //
   //
@@ -41,4 +52,9 @@ Future<void> initDependencies() async {
   serviceLocator.registerSingleton<SignUpUseCase>(SignUpUseCase());
 
   serviceLocator.registerSingleton<UserStatusUseCase>(UserStatusUseCase());
+
+  //Media
+  serviceLocator.registerSingleton<NewMediaUseCase>(NewMediaUseCase());
+
+  serviceLocator.registerSingleton<FetchMediasUseCase>(FetchMediasUseCase());
 }
