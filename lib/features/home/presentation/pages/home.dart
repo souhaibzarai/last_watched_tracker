cebbf761e488d42251d4feb189cef1128feb013c
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-01 17:41:47 +0100

    add changed header, add medias section

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -2,10 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:last_watched_tracker/common/widgets/scaffold/custom_app_scaffold.dart';
-import 'package:last_watched_tracker/features/home/presentation/widgets/custom_search_field.dart';
+import 'package:last_watched_tracker/features/home/presentation/widgets/header.dart';
+import 'package:last_watched_tracker/features/media/presentation/cubit/fetch_medias_cubit.dart';
 import 'package:last_watched_tracker/utils/constants/constants.dart';
 
 import '../cubit/user_info_cubit.dart';
+import '../widgets/medias.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,16 +17,16 @@ class HomePage extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => UserInfoCubit()..getUserInfo()),
+        BlocProvider(create: (context) => FetchMediasCubit()..fetchMedias()),
       ],
       child: CustomAppScaffold(
         child: SizedBox(
           width: double.infinity,
           child: Column(
             children: [
-              const CustomSearchField(),
-              AppConstants.mediumSizedBox,
-              AppConstants.getHorizontalDivider(isTransparent: false),
-              AppConstants.mediumSizedBox,
+              Header(),
+              AppConstants.verticalMediumSizedBox,
+              const Medias(),
             ],
           ),
         ),
