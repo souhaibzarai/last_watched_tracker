42da314b8577800e04f22002295a33b49e46ee3c
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-01 17:43:08 +0100

    new customization & a bit of new logic

--- lib/common/widgets/custom_text_field.dart
+++ lib/common/widgets/custom_text_field.dart
@@ -9,15 +9,21 @@ class CustomTextField extends StatelessWidget {
     required this.labelText,
     this.keyboardType,
     this.obscureText = false,
+    this.isDescriptive = false,
+    this.isOptional = false,
     this.prefixIcon,
     this.suffixIcon,
     this.validator,
+    this.hintText,
   });
 
   final TextEditingController? controller;
   final String labelText;
+  final String? hintText;
   final TextInputType? keyboardType;
   final bool obscureText;
+  final bool isDescriptive;
+  final bool isOptional;
   final IconData? prefixIcon;
   final Widget? suffixIcon;
   final String? Function(String?)? validator;
@@ -28,6 +34,9 @@ class CustomTextField extends StatelessWidget {
       validator:
           validator ??
           (value) {
+            if (isOptional) {
+              return null;
+            }
             if (value == null || value.isEmpty) {
               return 'Please enter a valid ${labelText.toLowerCase()}';
             }
@@ -35,16 +44,22 @@ class CustomTextField extends StatelessWidget {
           },
       controller: controller,
       keyboardType: keyboardType,
-      obscureText: obscureText,
+      obscureText: obscureText && !isDescriptive,
+      cursorColor: AppColors.infoColor,
+      cursorWidth: 1,
+      minLines: isDescriptive ? 3 : 1,
+      maxLines: isDescriptive ? 4 : 1,
+      cursorErrorColor: AppColors.errorColor,
       style: const TextStyle(color: AppColors.textColor),
       decoration: InputDecoration(
         constraints: BoxConstraints(),
         labelText: labelText,
         hintStyle: const TextStyle(color: Colors.white54),
-        prefixIcon: Icon(prefixIcon, color: Colors.white70),
+        prefixIcon:
+            prefixIcon != null ? Icon(prefixIcon, color: Colors.white70) : null,
         suffixIcon: suffixIcon,
         filled: true,
-
+        hintText: hintText,
         fillColor: AppColors.textColor.withAlpha(20),
         border: AppCommons.getOutlineInputBorder(),
         enabledBorder: AppCommons.getOutlineInputBorder(),
