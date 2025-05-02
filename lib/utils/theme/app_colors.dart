43b65982fc3dabddee1c7d566325a1660f100e3b
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-02 11:28:33 +0100

    new color

--- lib/utils/theme/app_colors.dart
+++ lib/utils/theme/app_colors.dart
@@ -9,6 +9,7 @@ class AppColors {
   static const Color errorColor = Color(0xFFEF4444);
   static const Color warningColor = Color(0xFFFACC15);
   static const Color successColor = Color(0xFF22C55E);
+  static const Color slidableGreenColor = Color(0xFF128C7E);
   static const Color buttonBgColor = Color(0xFF578FCA);
   static const Color textColor = Color(0xFFF5F5F2);
   static const Color disabledColor = Color(0xFF3E5879);
