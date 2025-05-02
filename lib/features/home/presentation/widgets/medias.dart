b29f716eebfec4b6f9c757fe853f2b57fda7104e
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-02 20:30:38 +0100

    refactoring

--- lib/features/home/presentation/widgets/medias.dart
+++ lib/features/home/presentation/widgets/medias.dart
@@ -1,35 +1,48 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../../../common/app_commons.dart';
-import '../../../media/presentation/cubit/fetch_medias_state.dart';
-import '../../../media/presentation/cubit/fetch_medias_cubit.dart';
-import '../../../media/presentation/widgets/media_item.dart';
 import '../../../../utils/messages/message_en.dart';
+import '../../../media/domain/mappers/media.dart';
+import '../../../media/presentation/cubit/fetch_medias_cubit.dart';
+import '../../../media/presentation/cubit/fetch_medias_state.dart';
+import '../../../media/presentation/widgets/media_item.dart';
 
 class Medias extends StatelessWidget {
-  const Medias({super.key});
+  const Medias({super.key, this.isArchived = false});
+
+  final bool isArchived;
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<FetchMediasCubit, MediaState>(
       builder: (context, state) {
         if (state is MediaLoadingState) {
-          AppCommons.centerProgressIndicator;
+          return SizedBox(
+            height: 100,
+            child: Center(child: AppCommons.centerProgressIndicator),
+          );
         } else if (state is MediaSuccessState) {
-          final medias = state.medias;
+          final medias = MediaMappers.getMedias(
+            state.medias,
+            isArchived: isArchived,
+          );
           if (medias.isEmpty) {
             return const SizedBox(
               height: 100,
-              child: Center(child: Text(CommonMessagesEn.mediaListEmpty)),
+              child: Center(
+                child: Text(
+                  CommonMessagesEn.mediaListEmpty,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
             );
           }
-          return Expanded(
-            child: ListView.builder(
-              itemCount: medias.length,
-              itemBuilder: (context, index) {
-                return MediaItem(mediaEntity: medias[index]);
-              },
-            ),
+          return ListView.builder(
+            itemCount: medias.length,
+            itemBuilder: (context, index) {
+              return MediaItem(media: medias[index]);
+            },
           );
         } else if (state is MediaFailureState) {
           AppCommons.showScaffold(context, message: state.err);
