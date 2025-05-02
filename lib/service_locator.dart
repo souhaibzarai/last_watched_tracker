e57ba72a50ee1cbc6b5d72b656f3095f3b195962
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-02 20:29:12 +0100

    archive func declared, di for firebase auth

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,25 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+
 import 'features/auth/data/repository/auth_repo_impl.dart';
 import 'features/auth/data/source/firebase_auth_source.dart';
+import 'features/auth/domain/repository/auth_repository.dart';
 import 'features/auth/domain/usecases/get_user.dart';
 import 'features/auth/domain/usecases/reset_password.dart';
 import 'features/auth/domain/usecases/sign_in.dart';
 import 'features/auth/domain/usecases/sign_up.dart';
 import 'features/auth/domain/usecases/user_status.dart';
+import 'features/media/data/repository/media_repo_impl.dart';
 import 'features/media/data/source/media_firebase_source.dart';
 import 'features/media/domain/repository/media_repo.dart';
 import 'features/media/domain/usecases/fetch_medias.dart';
 import 'features/media/domain/usecases/new_media.dart';
-
-import 'features/auth/domain/repository/auth_repository.dart';
-import 'features/media/data/repository/media_repo_impl.dart';
+import 'features/media/domain/usecases/toggle_archive.dart';
 
 final serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
+  serviceLocator.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
+
   // Services
   serviceLocator.registerSingleton<AuthFirebaseService>(
-    AuthFirebaseServiceImpl(),
+    AuthFirebaseServiceImpl(auth: serviceLocator<FirebaseAuth>()),
   );
 
   serviceLocator.registerSingleton<MediaFirebaseSource>(
@@ -57,4 +61,8 @@ Future<void> initDependencies() async {
   serviceLocator.registerSingleton<NewMediaUseCase>(NewMediaUseCase());
 
   serviceLocator.registerSingleton<FetchMediasUseCase>(FetchMediasUseCase());
+
+  serviceLocator.registerSingleton<ToggleArchiveUseCase>(
+    ToggleArchiveUseCase(),
+  );
 }
