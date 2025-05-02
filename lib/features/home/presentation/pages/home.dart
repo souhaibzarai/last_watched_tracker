fdbd3e194c99d28b036262aa525c6bd67ff867d4
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-02 20:30:16 +0100

    add archive related stuff

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:last_watched_tracker/common/widgets/scaffold/custom_app_scaffold.dart';
-import 'package:last_watched_tracker/features/home/presentation/widgets/header.dart';
-import 'package:last_watched_tracker/features/media/presentation/cubit/fetch_medias_cubit.dart';
-import 'package:last_watched_tracker/utils/constants/constants.dart';
 
+import '../../../../common/widgets/scaffold/custom_app_scaffold.dart';
+import '../../../../utils/constants/constants.dart';
 import '../cubit/user_info_cubit.dart';
+import '../widgets/archived.dart';
+import '../widgets/header.dart';
 import '../widgets/medias.dart';
 
 class HomePage extends StatelessWidget {
@@ -17,16 +17,16 @@ class HomePage extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => UserInfoCubit()..getUserInfo()),
-        BlocProvider(create: (context) => FetchMediasCubit()..fetchMedias()),
       ],
       child: CustomAppScaffold(
         child: SizedBox(
           width: double.infinity,
           child: Column(
             children: [
-              Header(),
+              const Header(),
               AppConstants.verticalMediumSizedBox,
-              const Medias(),
+              const Archived(),
+              Expanded(child: const Medias()),
             ],
           ),
         ),
