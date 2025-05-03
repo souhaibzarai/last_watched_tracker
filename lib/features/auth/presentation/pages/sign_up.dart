76ecf7a12ac081007aa47f940132191abe485388
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-03 18:36:53 +0100

    code refactoring

--- lib/features/auth/presentation/pages/sign_up.dart
+++ lib/features/auth/presentation/pages/sign_up.dart
@@ -57,11 +57,8 @@ class _SignUpPageState extends State<SignUpPage>
   Widget build(BuildContext context) {
     return CustomAuthScaffold(
       appBar: CustomAppBar(),
-      body: MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (context) => CheckCubit()),
-          BlocProvider(create: (context) => ButtonStateCubit()),
-        ],
+      body: BlocProvider(
+        create: (context) => ButtonStateCubit(),
         child: BlocListener<ButtonStateCubit, ButtonState>(
           listener: (context, state) {
             if (state is SuccessButtonState) {
@@ -75,116 +72,121 @@ class _SignUpPageState extends State<SignUpPage>
               padding: const EdgeInsets.all(12.0),
               child: Form(
                 key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CustomAuthText(
-                      title: 'Create Account',
-                      subTitle: 'Sign up to get started',
-                    ),
+                child: BlocProvider(
+                  create: (_) => CheckCubit(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const CustomAuthText(
+                        title: 'Create Account',
+                        subTitle: 'Sign up to get started',
+                      ),
 
-                    CustomTextField(
-                      controller: _usernameController,
-                      labelText: 'Username',
-                      prefixIcon: Icons.person_outline,
-                      keyboardType: TextInputType.name,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your username';
-                        }
-                        return null;
-                      },
-                    ),
-                    AppConstants.verticalMediumSizedBox,
+                      CustomTextField(
+                        controller: _usernameController,
+                        labelText: 'Username',
+                        prefixIcon: Icons.person_outline,
+                        keyboardType: TextInputType.name,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your username';
+                          }
+                          return null;
+                        },
+                      ),
+                      AppConstants.verticalMediumSizedBox,
 
-                    CustomTextField(
-                      controller: _emailController,
-                      labelText: 'Email',
-                      prefixIcon: Icons.email_outlined,
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value == null || !value.contains('@')) {
-                          return 'Please enter a valid email';
-                        }
-                        return null;
-                      },
-                    ),
-                    AppConstants.verticalMediumSizedBox,
+                      CustomTextField(
+                        controller: _emailController,
+                        labelText: 'Email',
+                        prefixIcon: Icons.email_outlined,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || !value.contains('@')) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                      ),
+                      AppConstants.verticalMediumSizedBox,
 
-                    PasswordTextField(
-                      controller: _passwordController,
-                      validator: (value) {
-                        if (value == null || value.length < 6) {
-                          return 'Password must be at least 6 characters';
-                        }
-                        return null;
-                      },
-                    ),
-                    AppConstants.verticalMediumSizedBox,
+                      PasswordTextField(
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value == null || value.length < 6) {
+                            return 'Password must be at least 6 characters';
+                          }
+                          return null;
+                        },
+                      ),
+                      AppConstants.verticalMediumSizedBox,
 
-                    PasswordTextField(
-                      controller: _confirmPasswordController,
-                      title: 'Confirm Password',
-                      validator: (value) {
-                        if (value != _passwordController.text) {
-                          return 'Passwords do not match';
-                        }
-                        return null;
-                      },
-                    ),
-                    AppConstants.verticalSmallSizedBox,
+                      PasswordTextField(
+                        controller: _confirmPasswordController,
+                        title: 'Confirm Password',
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Password must be at least 6 characters';
+                          } else if (value != _passwordController.text) {
+                            return 'Passwords do not match';
+                          }
+                          return null;
+                        },
+                      ),
+                      AppConstants.verticalSmallSizedBox,
 
-                    const TermsAndCond(),
+                      const TermsAndCond(),
 
-                    BlocBuilder<CheckCubit, bool>(
-                      builder: (context, isChecked) {
-                        if (isChecked) {
-                          return Column(
-                            children: [
-                              AppConstants.verticalSmallSizedBox,
-                              Builder(
-                                builder: (context) {
-                                  return CustomReactiveButton(
-                                    text: 'Sign Up',
-                                    onPressed: () async {
-                                      if (_formKey.currentState!.validate()) {
-                                        await context
-                                            .read<ButtonStateCubit>()
-                                            .execute(
-                                              usecase: SignUpUseCase(),
-                                              params: UserCreation(
-                                                username:
-                                                    _usernameController.text
-                                                        .trim(),
-                                                email:
-                                                    _emailController.text
-                                                        .trim(),
-                                                password:
-                                                    _passwordController.text
-                                                        .trim(),
-                                              ),
-                                            );
-                                      }
-                                    },
-                                  );
-                                },
-                              ),
-                              AppConstants.verticalMediumSizedBox,
-                            ],
-                          );
-                        }
-                        return const SizedBox.shrink();
-                      },
-                    ),
+                      BlocBuilder<CheckCubit, bool>(
+                        builder: (context, isChecked) {
+                          if (isChecked) {
+                            return Column(
+                              children: [
+                                AppConstants.verticalSmallSizedBox,
+                                Builder(
+                                  builder: (context) {
+                                    return CustomReactiveButton(
+                                      text: 'Sign Up',
+                                      onPressed: () async {
+                                        if (_formKey.currentState!.validate()) {
+                                          await context
+                                              .read<ButtonStateCubit>()
+                                              .execute(
+                                                usecase: SignUpUseCase(),
+                                                params: UserCreation(
+                                                  username:
+                                                      _usernameController.text
+                                                          .trim(),
+                                                  email:
+                                                      _emailController.text
+                                                          .trim(),
+                                                  password:
+                                                      _passwordController.text
+                                                          .trim(),
+                                                ),
+                                              );
+                                        }
+                                      },
+                                    );
+                                  },
+                                ),
+                                AppConstants.verticalMediumSizedBox,
+                              ],
+                            );
+                          }
+                          return const SizedBox.shrink();
+                        },
+                      ),
 
-                    UnderButtonText(
-                      text: "Already have an account?",
-                      clickableText: "Sign In",
-                      onTextClick: () {
-                        context.replace(NavigatorPath.login);
-                      },
-                    ),
-                  ],
+                      UnderButtonText(
+                        text: "Already have an account?",
+                        clickableText: "Sign In",
+                        onTextClick: () {
+                          context.replace(NavigatorPath.login);
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
