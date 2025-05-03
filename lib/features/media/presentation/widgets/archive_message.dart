cf601c061d2df8714d100e8b13e80188125583ed
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-03 10:17:51 +0100

    change color

--- lib/features/media/presentation/widgets/archive_message.dart
+++ lib/features/media/presentation/widgets/archive_message.dart
@@ -3,22 +3,20 @@ import 'package:last_watched_tracker/utils/messages/message_en.dart';
 import 'package:last_watched_tracker/utils/theme/app_colors.dart';
 
 class ArchiveMessage extends StatelessWidget {
-  const ArchiveMessage({
-    super.key,
-  });
+  const ArchiveMessage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color: AppColors.textColor.withAlpha(15), //
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: AppColors.secondaryTextColor, //
         border: Border.symmetric(
           horizontal: BorderSide(width: .1, color: AppColors.infoColor),
         ),
       ),
-      alignment: Alignment.centerLeft,
       padding: EdgeInsets.symmetric(horizontal: 35, vertical: 8),
-      child: Text(
+      child: const Text(
         CommonMessagesEn.mediaWillBeArchived,
         textAlign: TextAlign.center,
       ),
