758002701654e5c9d633cf2a100532309b01d4f0
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-03 16:08:01 +0100

    custom pressed field

---
+++ lib/common/widgets/button/custom_clickable_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:last_watched_tracker/utils/theme/app_colors.dart';
+
+class CustomClickableField extends StatelessWidget {
+  const CustomClickableField({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        border: Border.fromBorderSide(
+          const BorderSide(color: AppColors.infoColor, width: 1),
+        ),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(text),
+          const Icon(
+            Icons.keyboard_arrow_down_outlined,
+            color: AppColors.infoColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
