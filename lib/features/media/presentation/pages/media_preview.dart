39ffd31fa1cddac92479887773b94412463aef90
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-04 20:27:33 +0100

    Enhancing media preview Ui/Ux

--- lib/features/media/presentation/pages/media_preview.dart
+++ lib/features/media/presentation/pages/media_preview.dart
@@ -2,12 +2,15 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:last_watched_tracker/utils/constants/constants.dart';
 
 import '../../../../common/widgets/text/custom_text.dart';
+import '../../../../utils/constants/constants.dart';
+import '../../../../utils/helpers/strings/percentage_helper.dart';
 import '../../../../utils/theme/app_colors.dart';
 import '../../domain/entities/media.dart';
+import '../widgets/media_status_preview.dart';
 import '../widgets/media_text_preview.dart';
+import '../widgets/media_title_preview.dart';
 
 class MediaDetailsPreview extends StatelessWidget {
   const MediaDetailsPreview({super.key, required this.media});
@@ -46,33 +49,56 @@ class MediaDetailsPreview extends StatelessWidget {
             Positioned(
               right: 0,
               left: 0,
-              bottom: 100,
+              bottom: -1,
               child: ClipRRect(
                 child: BackdropFilter(
                   filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
                   child: Container(
                     color: AppColors.primaryColor.withAlpha(180),
-                    padding: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 20,
+                    ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        CustomText(media.title),
+                        AppConstants.verticalSmallSizedBox,
                         Row(
                           children: [
-                            Expanded(
-                              child: FractionallySizedBox(
-                                alignment: Alignment.centerLeft,
-                                widthFactor: 1.5,
-                                child: CustomText(media.title),
+                            MediaPreviewTitle(media: media),
+                            const Spacer(),
+                            MediaPreviewStatus(media: media),
+                          ],
+                        ),
+                        AppConstants.verticalSmallSizedBox,
+                        FractionallySizedBox(
+                          widthFactor: 1,
+                          child: Container(
+                            height: 10,
+                            width: double.infinity,
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                              color: AppColors.previewTextBgColor,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: FractionallySizedBox(
+                              widthFactor:
+                                  (PercentageHelper.getPercentage(
+                                        media.progress,
+                                        media.total,
+                                      ) /
+                                      100),
+                              alignment: Alignment.centerLeft,
+                              child: Container(
+                                clipBehavior: Clip.hardEdge,
+                                decoration: BoxDecoration(
+                                  color: AppColors.secondaryColor,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
                             ),
-                            const Spacer(),
-                            CustomText(
-                              media.status,
-                              size: 12,
-                              fw: FontWeight.w300,
-                              color: AppColors.secondaryColor,
-                            ),
-                          ],
+                          ),
                         ),
                         AppConstants.verticalSmallSizedBox,
                         (media.notes == null || media.notes!.isEmpty)
