19df6965bdd38d1d08d64adc4fa70669a58d70d3
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-05 12:52:25 +0100

    Method to implement number of chapters read by user

--- lib/utils/helpers/strings/string_helper.dart
+++ lib/utils/helpers/strings/string_helper.dart
@@ -5,4 +5,15 @@ class StringHelper {
     return trimedPhrase[0].toUpperCase() +
         trimedPhrase.substring(1).toLowerCase().trim();
   }
+
+  static String getChaptersTextCount(String progress, String? total) {
+    String totalCount = '?';
+    if (total == '' || total == null || total.isEmpty) {
+      totalCount = '?';
+    } else {
+      totalCount = total;
+    }
+
+    return '$progress / $totalCount chapters';
+  }
 }
