4556a5c38aff093b03c25233a8b34835d73ad406
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-05 15:35:30 +0100

    media details page declaration on router

--- lib/go_router.dart
+++ lib/go_router.dart
@@ -9,8 +9,10 @@ import 'features/auth/presentation/pages/sign_up.dart';
 import 'features/category/presentation/cubit/category_cubit.dart';
 import 'features/category/presentation/cubit/category_selector_cubit.dart';
 import 'features/home/presentation/pages/home.dart';
+import 'features/media/domain/entities/media.dart';
 import 'features/media/presentation/pages/add_media.dart';
 import 'features/media/presentation/pages/archive.dart';
+import 'features/media/presentation/pages/media_details.dart';
 import 'features/splash/presentation/splash.dart';
 import 'features/status/presentation/cubit/status_cubit.dart';
 import 'features/status/presentation/cubit/status_selector_cubit.dart';
@@ -68,6 +70,13 @@ GoRouter router = GoRouter(
         GoRoute(
           path: NavigatorPath.archive,
           builder: (context, state) => const ArchivePage(),
+        ),
+        GoRoute(
+          path: NavigatorPath.mediaDetails,
+          builder: (context, state) {
+            final media = state.extra as MediaEntity;
+            return MediaDetailsPage(media: media);
+          },
         ),
       ],
     ),
