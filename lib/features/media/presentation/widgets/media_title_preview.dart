6bca3a2ede0cc2349a05e5cd5261ae452974d6be
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-05 12:48:28 +0100

    Refactoring code and fixed missing widgets

--- lib/features/media/presentation/widgets/media_title_preview.dart
+++ lib/features/media/presentation/widgets/media_title_preview.dart
@@ -1,29 +1,15 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/text/custom_text.dart';
-import '../../../../utils/theme/app_colors.dart';
 import '../../domain/entities/media.dart';
 
 class MediaPreviewTitle extends StatelessWidget {
-  const MediaPreviewTitle({
-    super.key,
-    required this.media,
-  });
+  const MediaPreviewTitle({super.key, required this.media});
 
   final MediaEntity media;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: FractionallySizedBox(
-        alignment: Alignment.centerLeft,
-        widthFactor: 1.5,
-        child: CustomText(
-          media.status,
-          color: AppColors.secondaryColor,
-          size: 14,
-        ),
-      ),
-    );
+    return CustomText(media.title, size: 16, fw: FontWeight.bold);
   }
 }
