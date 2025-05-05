fccb9beaded182c9a7d81f7b1111cefd2881ded2
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-05 12:49:17 +0100

    color changes

--- lib/utils/theme/app_colors.dart
+++ lib/utils/theme/app_colors.dart
@@ -12,7 +12,7 @@ class AppColors {
   static const Color slidableGreenColor = Color(0xFF128C7E);
   static const Color buttonBgColor = Color(0xFF578FCA);
   static const Color textColor = Color(0xFFF5F5F2);
-  static const Color secondaryTextColor = Color.fromARGB(15, 245, 245, 242);
+  static const Color secondaryTextColor = Color.fromARGB(100, 245, 245, 242);
   static const Color disabledColor = Color(0xFF3E5879);
-  static const Color previewTextBgColor = Color(0xFF1d2939);
+  static const Color previewTextBgColor = Color.fromARGB(85, 29, 41, 57);
 }
