4d99ff6f16b10b4264f78c39b579239c1101e834
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-05 17:44:47 +0100

    Ui changes, and logic to navigate to media details from preview

--- lib/features/media/presentation/pages/media_preview.dart
+++ lib/features/media/presentation/pages/media_preview.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:last_watched_tracker/utils/helpers/navigator/strings.dart';
 
 import '../../../../common/widgets/blured_bg_preview.dart';
 import '../../../../utils/constants/constants.dart';
@@ -20,51 +22,60 @@ class MediaDetailsPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext kContext) {
-    return BluredBgPreview(
-      child: Stack(
-        clipBehavior: Clip.hardEdge,
-        children: [
-          Image.network(
-            media.imgUrl,
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            right: 10,
-            top: 10,
-            child: MediaTextPreview(text: media.category),
-          ),
-          Positioned(
-            right: 0,
-            left: 0,
-            bottom: -1,
-            child: ClipRRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
-                child: Container(
-                  color: AppColors.primaryColor.withAlpha(180),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      MediaPreviewTitle(media: media),
-                      AppConstants.verticalVerySmallSizedBox,
-                      MediaPreviewStatus(media: media),
-                      AppConstants.verticalVerySmallSizedBox,
-                      MediaStatusPercentageBar(media: media),
-                      AppConstants.verticalSmallSizedBox,
-                      ChaptersTextCount(media: media),
-                    ],
+    return GestureDetector(
+      onTap: () {
+        kContext.push(NavigatorPath.mediaDetails, extra: media);
+        kContext.pop();
+      },
+      child: BluredBgPreview(
+        child: Stack(
+          clipBehavior: Clip.hardEdge,
+          children: [
+            Hero(
+              tag: media,
+              child: Image.network(
+                media.imgUrl,
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              right: 10,
+              top: 10,
+              child: MediaTextPreview(text: media.category),
+            ),
+            Positioned(
+              right: 0,
+              left: 0,
+              bottom: -1,
+              child: ClipRRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                  child: Container(
+                    color: AppColors.primaryColor.withAlpha(180),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MediaPreviewTitle(media: media),
+                        AppConstants.verticalVerySmallSizedBox,
+                        MediaPreviewStatus(media: media),
+                        AppConstants.verticalVerySmallSizedBox,
+                        MediaStatusPercentageBar(media: media),
+                        AppConstants.verticalSmallSizedBox,
+                        ChaptersTextCount(media: media),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
