6fee39e53be50bd5df63475e3f1023fd255c659f
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-06 18:03:01 +0100

    refactor code

---
+++ lib/features/media/presentation/widgets/archive_media_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../common/widgets/button/custom_media_details_button.dart';
+import '../../data/models/archive.dart';
+import '../../domain/entities/media.dart';
+import '../cubit/check_archive_cubit.dart';
+import '../../../../utils/helpers/functions/functions.dart';
+import '../../../../utils/theme/app_colors.dart';
+
+class ArchiveMediaButton extends StatelessWidget {
+  const ArchiveMediaButton({super.key, required this.media});
+
+  final MediaEntity media;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CheckArchiveCubit()..setArchiveStatus(media.isArchived),
+      child: Builder(
+        builder: (context) {
+          return CustomMediaDetailsButton(
+            bgColor: AppColors.slidableGreenColor,
+            icon:
+                context.watch<CheckArchiveCubit>().state == true
+                    ? Icons.unarchive
+                    : CupertinoIcons.archivebox_fill,
+            onPressed: () {
+              final checkCubit = context.read<CheckArchiveCubit>();
+              final newStatus = checkCubit.state;
+              FunctionsHelper.toggleArchive(
+                context,
+                ArchiveModel(id: media.id, status: newStatus),
+              );
+              checkCubit.setArchiveStatus(!newStatus);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
