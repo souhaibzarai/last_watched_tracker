0c59cf45c2e7beeb932908b15502133b5c7900df
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-06 08:58:36 +0100

    remove useless paths

--- lib/utils/constants/images_path.dart
+++ lib/utils/constants/images_path.dart
@@ -6,16 +6,11 @@ class ImagesPath {
   static const String _imgExt = '.png';
 
   static const String splashImage = 'assets/splash/$_splashImageName$_imgExt';
+
   static const String emailSent =
       'assets/$_imageMainFolderPathName/email_sent$_imgExt';
-  static const String loadingAnimation =
-      'assets/$_imageMainFolderPathName/loading.gif';
-  static const String defaultProfile =
-      'assets/$_imageMainFolderPathName/default_pic$_imgExt';
 
-  static const String notFoundImg =
-      'assets/$_imageMainFolderPathName/product-not-found$_imgExt';
-  //
+  static const String logo = 'assets/$_imageMainFolderPathName/logo$_imgExt';
 
   static const String successImage =
       'assets/$_imageMainFolderPathName/success$_imgExt';
