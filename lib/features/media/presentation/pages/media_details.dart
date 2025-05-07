fa1f5aacca65bcdb7003a9b9740c23a10f50aa7b
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-07 11:35:45 +0100

    Personal milestone: CustomScrollView realization
    On 06/05/2025, I realized how important CustomScrollView is for smooth scroll behavior. This commit may not matter to others, but it marks a meaningful moment in my learning journey.

--- lib/features/media/presentation/pages/media_details.dart
+++ lib/features/media/presentation/pages/media_details.dart
@@ -1,20 +1,19 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../common/widgets/button/custom_back_button.dart';
-import '../../../../common/widgets/button/custom_media_details_button.dart';
 import '../../../../common/widgets/scaffold/custom_app_scaffold.dart';
 import '../../../../common/widgets/text/custom_text.dart';
 import '../../../../utils/constants/constants.dart';
 import '../../../../utils/helpers/numbers/percentage_helper.dart';
 import '../../../../utils/theme/app_colors.dart';
 import '../../domain/entities/media.dart';
-import '../widgets/archive_media_button.dart';
+import '../cubit/check_archive_cubit.dart';
 import '../widgets/chapters_text_count.dart';
 import '../widgets/media_notes.dart';
 import '../widgets/media_status_percentage_bar.dart';
-import '../widgets/media_title_preview.dart';
 import '../widgets/scrollable_media_details_section.dart';
 
 class MediaDetailsPage extends StatelessWidget {
@@ -25,88 +24,96 @@ class MediaDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomAppScaffold(
-      child: Column(
-        children: [
-          CustomBackButton(),
-          Expanded(
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverMediaNavBar(media: media),
+          SliverToBoxAdapter(
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AppConstants.verticalSmallSizedBox,
-                    Center(
-                      child: Hero(
-                        tag: media,
-                        child: CircleAvatar(
-                          minRadius: 80,
-                          maxRadius: 120,
-                          backgroundImage: NetworkImage(
-                            media.imgUrl, //
-                          ),
-                        ),
+              padding: AppConstants.smallPadding,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Hero(
+                    tag: media,
+                    child: CircleAvatar(
+                      minRadius: 40,
+                      maxRadius: 80,
+                      backgroundImage: NetworkImage(
+                        media.imgUrl, //
                       ),
                     ),
-                    AppConstants.verticalVerySmallSizedBox,
-                    MediaPreviewTitle(media: media),
-                    AppConstants.verticalSmallSizedBox,
-                    ScrollableMediaDetailsSection(media: media),
-                    AppConstants.verticalSmallSizedBox,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ChaptersTextCount(media: media),
-                        CustomText(
-                          '${PercentageHelper.getPercentage(media.progress, media.total)}%',
-                          size: 12,
-                          fw: FontWeight.w300,
-                          color: AppColors.textColor,
-                        ),
-                      ],
-                    ),
-                    AppConstants.verticalVerySmallSizedBox,
-                    MediaStatusPercentageBar(media: media),
-                    AppConstants.verticalMediumSizedBox,
-                    if ((media.notes ?? '').isNotEmpty)
-                      MediaNotes(media: media),
-                    if ((media.notes ?? '').isNotEmpty)
-                      AppConstants.verticalLargeSizedBox,
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          CustomMediaDetailsButton(
-                            icon: CupertinoIcons.delete_solid,
-                            color: AppColors.textColor,
-                            bgColor: AppColors.errorColor,
-                            onPressed: () {
-                              print('Delete media');
-                              if (context.canPop()) {
-                                context.pop();
-                              }
-                            },
-                          ),
-                          AppConstants.horizontalMediumSizedBox,
-                          CustomMediaDetailsButton(
-                            bgColor: AppColors.infoColor,
-                            color: AppColors.primaryColor,
-                            icon: Icons.edit,
-                            onPressed: () {
-                              print('Update media');
-                            },
-                          ),
-                          AppConstants.horizontalMediumSizedBox,
-                          ArchiveMediaButton(media: media),
-                        ],
+                  ),
+                  AppConstants.verticalSmallSizedBox,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ChaptersTextCount(media: media),
+                      CustomText(
+                        '${PercentageHelper.getPercentage(media.progress, media.total)}%',
+                        size: 12,
+                        fw: FontWeight.w300,
+                        color: AppColors.textColor,
                       ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                  AppConstants.verticalVerySmallSizedBox,
+                  MediaStatusPercentageBar(media: media),
+                  AppConstants.verticalSmallSizedBox,
+                  BlocProvider(
+                    create:
+                        (context) =>
+                            CheckArchiveCubit()
+                              ..setArchiveStatus(media.isArchived),
+                    child: ScrollableMediaDetailsSection(media: media),
+                  ),
+                ],
               ),
             ),
           ),
+          if ((media.notes ?? '').isNotEmpty)
+            SliverToBoxAdapter(child: MediaNotes(media: media)),
+
+          SliverAppBar(
+            toolbarHeight: 800,
+            backgroundColor: AppColors.primaryColor,
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class SliverMediaNavBar extends StatelessWidget {
+  const SliverMediaNavBar({super.key, required this.media});
+
+  final MediaEntity media;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      snap: true,
+      floating: true,
+      stretch: true,
+      backgroundColor: Colors.transparent,
+      automaticallyImplyLeading: false,
+      leading: CustomBackButton(),
+      flexibleSpace: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+          child: FlexibleSpaceBar(
+            title: CustomText(
+              media.title,
+              color: AppColors.textColor,
+              fw: FontWeight.w500,
+            ),
+            centerTitle: true,
+            stretchModes: [
+              StretchMode.fadeTitle, //
+              StretchMode.zoomBackground,
+            ],
+          ),
+        ),
       ),
     );
   }
