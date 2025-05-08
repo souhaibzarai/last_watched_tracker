3dc14f16752ec96884862c413aaacd4ace7f1309
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-08 13:35:10 +0100

    playing around with slivers

--- lib/features/media/presentation/pages/media_details.dart
+++ lib/features/media/presentation/pages/media_details.dart
@@ -1,19 +1,13 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../common/widgets/button/custom_back_button.dart';
 import '../../../../common/widgets/scaffold/custom_app_scaffold.dart';
 import '../../../../common/widgets/text/custom_text.dart';
-import '../../../../utils/constants/constants.dart';
-import '../../../../utils/helpers/numbers/percentage_helper.dart';
 import '../../../../utils/theme/app_colors.dart';
 import '../../domain/entities/media.dart';
 import '../cubit/check_archive_cubit.dart';
-import '../widgets/chapters_text_count.dart';
+import '../widgets/custom_sliver_media_details_header.dart';
 import '../widgets/media_notes.dart';
-import '../widgets/media_status_percentage_bar.dart';
 import '../widgets/scrollable_media_details_section.dart';
 
 class MediaDetailsPage extends StatelessWidget {
@@ -24,95 +18,89 @@ class MediaDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomAppScaffold(
-      child: CustomScrollView(
-        physics: const BouncingScrollPhysics(),
-        slivers: [
-          SliverMediaNavBar(media: media),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: AppConstants.smallPadding,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      child: BlocProvider<CheckArchiveCubit>(
+        lazy: true,
+        create:
+            (context) =>
+                CheckArchiveCubit()..setArchiveStatus(media.isArchived),
+        child: CustomScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          slivers: [
+            CustomSliverMediaDetailsHeader(media: media),
+            SliverToBoxAdapter(
+              child: Stack(
                 children: [
+                  // Image place
                   Hero(
                     tag: media,
-                    child: CircleAvatar(
-                      minRadius: 40,
-                      maxRadius: 80,
-                      backgroundImage: NetworkImage(
-                        media.imgUrl, //
+                    child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      height: 400,
+                      alignment: Alignment.center,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40),
+                        ),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            media.imgUrl, //
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                  AppConstants.verticalSmallSizedBox,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ChaptersTextCount(media: media),
-                      CustomText(
-                        '${PercentageHelper.getPercentage(media.progress, media.total)}%',
-                        size: 12,
-                        fw: FontWeight.w300,
-                        color: AppColors.textColor,
+                  // title placeholder
+                  Positioned(
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 12,
                       ),
-                    ],
-                  ),
-                  AppConstants.verticalVerySmallSizedBox,
-                  MediaStatusPercentageBar(media: media),
-                  AppConstants.verticalSmallSizedBox,
-                  BlocProvider(
-                    create:
-                        (context) =>
-                            CheckArchiveCubit()
-                              ..setArchiveStatus(media.isArchived),
-                    child: ScrollableMediaDetailsSection(media: media),
+                      alignment: Alignment.bottomLeft,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(100),
+                        ),
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.center,
+                          colors: [
+                            AppColors.primaryColor,
+                            AppColors.primaryColor.withAlpha(180),
+                            Colors.transparent,
+                          ],
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CustomText(media.title, size: 22),
+                          ScrollableMediaDetailsSection(media: media),
+                        ],
+                      ),
+                    ), //
                   ),
                 ],
               ),
             ),
-          ),
-          if ((media.notes ?? '').isNotEmpty)
-            SliverToBoxAdapter(child: MediaNotes(media: media)),
 
-          SliverAppBar(
-            toolbarHeight: 800,
-            backgroundColor: AppColors.primaryColor,
-          ),
-        ],
-      ),
-    );
-  }
-}
+            if ((media.notes ?? '').isNotEmpty)
+              SliverToBoxAdapter(child: MediaNotes(media: media)),
 
-class SliverMediaNavBar extends StatelessWidget {
-  const SliverMediaNavBar({super.key, required this.media});
-
-  final MediaEntity media;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverAppBar(
-      snap: true,
-      floating: true,
-      stretch: true,
-      backgroundColor: Colors.transparent,
-      automaticallyImplyLeading: false,
-      leading: CustomBackButton(),
-      flexibleSpace: ClipRRect(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
-          child: FlexibleSpaceBar(
-            title: CustomText(
-              media.title,
-              color: AppColors.textColor,
-              fw: FontWeight.w500,
+            SliverAppBar(
+              toolbarHeight: 800,
+              backgroundColor: AppColors.primaryColor,
             ),
-            centerTitle: true,
-            stretchModes: [
-              StretchMode.fadeTitle, //
-              StretchMode.zoomBackground,
-            ],
-          ),
+          ],
         ),
       ),
     );
