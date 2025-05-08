2189b25cd2c16553e819cd1fba616fa50922b543
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-08 17:18:09 +0100

    ui changes

--- lib/features/media/presentation/widgets/header_sliver_media_details.dart
+++ lib/features/media/presentation/widgets/header_sliver_media_details.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/button/custom_back_button.dart';
-import '../../../../common/widgets/button/custom_media_details_button.dart';
-import '../../../../utils/constants/constants.dart';
 import '../../../../utils/theme/app_colors.dart';
 import '../../domain/entities/media.dart';
-import 'archive_media_button.dart';
 
 class HeaderSliverMediaDetails extends StatelessWidget {
   const HeaderSliverMediaDetails({super.key, required this.media});
@@ -31,13 +28,6 @@ class HeaderSliverMediaDetails extends StatelessWidget {
               color: AppColors.buttonBgColor,
               borderRadius: BorderRadius.circular(12),
               child: const CustomBackButton(color: AppColors.primaryColor),
-            ),
-            Row(
-              children: [
-                ArchiveMediaButton(media: media),
-                AppConstants.horizontalSmallSizedBox,
-                CustomMediaDetailsButton(icon: Icons.menu, onPressed: () {}),
-              ],
             ),
           ],
         ),
