a72233f397b87f1d13975e8c9aebbfd8c1d2530e
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-08 13:39:27 +0100

    fix imports + rm bg color

--- lib/features/media/presentation/widgets/archive_media_button.dart
+++ lib/features/media/presentation/widgets/archive_media_button.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../../../common/widgets/button/custom_media_details_button.dart';
+import '../../../../utils/helpers/functions/functions.dart';
+import '../../../../utils/theme/app_colors.dart';
 import '../../data/models/archive.dart';
 import '../../domain/entities/media.dart';
 import '../cubit/check_archive_cubit.dart';
-import '../../../../utils/helpers/functions/functions.dart';
-import '../../../../utils/theme/app_colors.dart';
 
 class ArchiveMediaButton extends StatelessWidget {
   const ArchiveMediaButton({super.key, required this.media});
@@ -20,7 +21,7 @@ class ArchiveMediaButton extends StatelessWidget {
       child: Builder(
         builder: (context) {
           return CustomMediaDetailsButton(
-            bgColor: AppColors.slidableGreenColor,
+            bgColor: AppColors.previewTextBgColor,
             icon:
                 context.watch<CheckArchiveCubit>().state == true
                     ? Icons.unarchive
