227da84515a9715ae5ce0d6538196ad6bf015488
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-08 13:33:18 +0100

    ui customization

--- lib/common/widgets/button/custom_back_button.dart
+++ lib/common/widgets/button/custom_back_button.dart
@@ -5,27 +5,31 @@ import 'package:go_router/go_router.dart';
 import '../../../utils/theme/app_colors.dart';
 
 class CustomBackButton extends StatelessWidget {
-  const CustomBackButton({super.key});
+  const CustomBackButton({super.key, this.color = AppColors.textColor});
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: InkWell(
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(12),
-          bottomRight: Radius.circular(12),
-        ),
-        onTap: () {
-          if (context.canPop()) {
-            context.pop();
-          }
-        },
-        child: Container(
-          width: 40,
-          height: 40,
-          padding: EdgeInsets.all(8),
-          child: Icon(CupertinoIcons.back, color: AppColors.textColor),
+    return SizedBox(
+      height: 40,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: InkWell(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(12),
+            bottomRight: Radius.circular(12),
+          ),
+          onTap: () {
+            if (context.canPop()) {
+              context.pop();
+            }
+          },
+          child: Container(
+            width: 40,
+            height: 40,
+            padding: EdgeInsets.all(8),
+            child: Icon(CupertinoIcons.back, color: color),
+          ),
         ),
       ),
     );
