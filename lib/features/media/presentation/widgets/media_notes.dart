c28b42b817255bac2e9c3cbb032596647cc52a04
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-08 13:37:39 +0100

    useless widget for now

--- lib/features/media/presentation/widgets/media_notes.dart
+++ lib/features/media/presentation/widgets/media_notes.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../../utils/constants/constants.dart';
 import '../../../../utils/theme/app_colors.dart';
 import '../../domain/entities/media.dart';
 
@@ -11,32 +10,25 @@ class MediaNotes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        AppConstants.getNotesDivider(endIndent: 50),
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: Text.rich(
-            textAlign: TextAlign.justify,
-            TextSpan(
-              style: const TextStyle(color: AppColors.textColor, fontSize: 14),
-              children: [
-                const TextSpan(
-                  text: '➾ ',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.secondaryColor,
-                  ),
-                ),
-                TextSpan(text: media.notes!),
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Text.rich(
+        textAlign: TextAlign.justify,
+        TextSpan(
+          style: const TextStyle(color: AppColors.textColor, fontSize: 14),
+          children: [
+            const TextSpan(
+              text: '➾ ',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: AppColors.secondaryColor,
+              ),
             ),
-          ),
+            TextSpan(text: media.notes!),
+          ],
         ),
-        AppConstants.getNotesDivider(indent: 50),
-      ],
+      ),
     );
   }
 }
