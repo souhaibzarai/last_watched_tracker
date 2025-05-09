d25794434b5db6e6de2d9c535fb77f9c5f1a2b80
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-09 17:05:30 +0100

    Delete media implementation and code refactoring

--- lib/features/media/presentation/widgets/custom_floating_bottom_bar.dart
+++ lib/features/media/presentation/widgets/custom_floating_bottom_bar.dart
@@ -2,11 +2,18 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../common/app_commons.dart';
 import '../../../../common/widgets/button/custom_media_details_button.dart';
 import '../../../../utils/theme/app_colors.dart';
 import '../../domain/entities/media.dart';
+import '../cubit/delete_media_cubit.dart';
+import '../cubit/delete_media_state.dart';
+import '../cubit/fetch_medias_cubit.dart';
 import 'archive_media_button.dart' show ArchiveMediaButton;
+import 'delete_media_confirm_dialog.dart';
 
 class CustomFloatingBottomBar extends StatelessWidget {
   const CustomFloatingBottomBar({super.key, required this.media});
@@ -38,32 +45,11 @@ class CustomFloatingBottomBar extends StatelessWidget {
           clipper: MyCustomClipper(),
           child: BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
-            child: SizedBox(),
+            child: const SizedBox(),
           ),
         ),
       ),
     );
-  }
-}
-
-class MyCustomClipper extends CustomClipper<Path> {
-  @override
-  getClip(Size size) {
-    final path = Path();
-
-    path.moveTo(30, size.height - 30);
-    path.lineTo(10, 0);
-    path.lineTo(size.width, 10);
-    path.lineTo(40, size.width);
-    path.lineTo(180, size.height);
-    path.lineTo(0, size.width);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper oldClipper) {
-    return true;
   }
 }
 
@@ -88,12 +74,61 @@ class CustomFloatingBNBActions extends StatelessWidget {
           onPressed: () {},
           color: AppColors.textColor,
         ),
-        CustomMediaDetailsButton(
-          icon: CupertinoIcons.delete,
-          onPressed: () {},
-          color: AppColors.textColor,
+        BlocProvider<DeleteMediaCubit>(
+          create: (context) => DeleteMediaCubit(),
+          lazy: true,
+          child: BlocListener<DeleteMediaCubit, DeleteMediaState>(
+            listener: (context, state) {
+              if (state is MediaDeleteFailed) {
+                return AppCommons.showScaffold(context, message: state.err);
+              } else if (state is MediaDeleteSuccess) {
+                context.read<FetchMediasCubit>().fetchMedias();
+                context.pop();
+              }
+            },
+            child: Builder(
+              builder: (context) {
+                return CustomMediaDetailsButton(
+                  icon: CupertinoIcons.delete,
+                  onPressed: () async {
+                    final result = await showDialog(
+                      context: context,
+                      builder: (dContext) {
+                        return DeleteMediaConfirmDialog(media: media);
+                      },
+                    );
+                    if (result == true && context.mounted) {
+                      context.read<DeleteMediaCubit>().deleteMedia(media.id);
+                    }
+                  },
+                  color: AppColors.textColor,
+                );
+              },
+            ),
+          ),
         ),
       ],
     );
+  }
+}
+
+class MyCustomClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    final path = Path();
+
+    path.moveTo(30, size.height - 30);
+    path.lineTo(10, 0);
+    path.lineTo(size.width, 10);
+    path.lineTo(40, size.width);
+    path.lineTo(180, size.height);
+    path.lineTo(0, size.width);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) {
+    return true;
   }
 }
