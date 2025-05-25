735a85b86bc4c627d3b4e70e7a53fb36b3637be0
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-25 11:53:25 +0100

    refactoring paths

--- lib/utils/constants/images_path.dart
+++ lib/utils/constants/images_path.dart
@@ -1,21 +1,21 @@
 class ImagesPath {
   ImagesPath._();
 
-  static const String _splashImageName = 'splash';
+  static const String _splashImg = 'splash';
   static const String _defaultImg = 'default';
-  static const String _imageMainFolderPathName = 'images';
-  static const String _imgExt = '.png';
+  static const String _images = 'images';
+  static const String _pngExt = '.png';
+  static const String _icons = 'icons';
 
-  static const String splashImage = 'assets/splash/$_splashImageName$_imgExt';
+  static const String splashImage = 'assets/splash/$_splashImg$_pngExt';
 
-  static const String emailSent =
-      'assets/$_imageMainFolderPathName/email_sent$_imgExt';
+  static const String emailSent = 'assets/$_images/email_sent$_pngExt';
 
-  static const String logo = 'assets/$_imageMainFolderPathName/logo$_imgExt';
+  static const String logo = 'assets/$_images/logo$_pngExt';
 
-  static const String defaultImg =
-      'assets/$_imageMainFolderPathName/$_defaultImg$_imgExt';
+  static const String defaultImg = 'assets/$_images/$_defaultImg$_pngExt';
 
-  static const String successImage =
-      'assets/$_imageMainFolderPathName/success$_imgExt';
+  static const String successImage = 'assets/$_images/success$_pngExt';
+
+  static const String menuIcon = 'assets/$_icons/menu$_pngExt';
 }
