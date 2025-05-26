61d3bc1b0cd3bb3873d302dfefd3f5f5d5e0a6f3
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-26 21:09:48 +0100

    refactoring / ui fixing

--- lib/common/widgets/button/bottom_sheet_custom_button_w_icon.dart
+++ lib/common/widgets/button/bottom_sheet_custom_button_w_icon.dart
@@ -1,32 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:last_watched_tracker/common/widgets/image/custom_image_asset.dart';
-import 'package:last_watched_tracker/utils/constants/images_path.dart';
-import 'package:last_watched_tracker/utils/theme/app_colors.dart';
+
+import '../../../utils/theme/app_colors.dart';
+import '../image/custom_image_asset.dart';
 
 class BottomSheetCustomButtonWIcon extends StatelessWidget {
-  const BottomSheetCustomButtonWIcon({super.key});
+  const BottomSheetCustomButtonWIcon({
+    super.key,
+    required this.path,
+    this.height = 32,
+    required this.title,
+    this.fSize = 20,
+    required this.colors,
+    required this.onPressed,
+  });
+
+  final String path;
+  final String title;
+  final double height;
+  final double fSize;
+  final List<Color> colors;
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10).r,
-      decoration: BoxDecoration(
+      margin: const EdgeInsets.symmetric(vertical: 5).r,
+      child: InkWell(
+        splashColor: AppColors.secondaryColor,
+        enableFeedback: false,
+        onTap: onPressed,
         borderRadius: BorderRadius.circular(20).r,
-        gradient: LinearGradient(
-          colors: [
-            AppColors.buttonBgColor.withAlpha(120),
-            AppColors.infoColor.withAlpha(120),
-            AppColors.secondaryColor.withAlpha(120),
-          ],
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10).r,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20).r,
+            gradient: LinearGradient(
+              colors: colors,
+              begin: Alignment.topLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title, style: TextStyle(fontSize: fSize.sp)),
+              CustomImageAsset(height: height.h, path: path),
+            ],
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Sign Out', style: TextStyle(fontSize: 18.sp)),
-          CustomImageAsset(height: 38.h, path: ImagesPath.logoutIcon),
-        ],
       ),
     );
   }
