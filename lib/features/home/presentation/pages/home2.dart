65c8a50c4a265c66faa5e7c583769d6462735229
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-26 21:10:14 +0100

    working on home screen ui

--- lib/features/home/presentation/pages/home2.dart
+++ lib/features/home/presentation/pages/home2.dart
@@ -3,10 +3,13 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:last_watched_tracker/common/widgets/button/bottom_sheet_custom_button_w_icon.dart';
+import 'package:go_router/go_router.dart';
+import 'package:last_watched_tracker/features/auth/domain/usecases/sign_out.dart';
+import 'package:last_watched_tracker/service_locator.dart';
+import 'package:last_watched_tracker/utils/helpers/navigator/strings.dart';
 
+import '../../../../common/widgets/button/bottom_sheet_custom_button_w_icon.dart';
 import '../../../../common/widgets/button/custom_img_icon_button.dart';
-import '../../../../common/widgets/image/custom_image_asset.dart';
 import '../../../../common/widgets/scaffold/custom_app_scaffold.dart';
 import '../../../../utils/constants/constants.dart';
 import '../../../../utils/constants/images_path.dart';
@@ -35,69 +38,111 @@ class Home2 extends StatelessWidget {
               floating: true,
               snap: true,
               flexibleSpace: FlexibleSpaceBar(
-                background: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8).r,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(child: CustomSearchField()),
-                      AppConstants.horizontalSmallSizedBox,
-                      CustomImgIconButton(
-                        path: ImagesPath.menuIcon,
-                        onPressed: () {
-                          showModalBottomSheet(
-                            context: context,
-                            useSafeArea: true,
-                            constraints: BoxConstraints(maxHeight: 400.h),
-                            sheetAnimationStyle: AnimationStyle(
-                              curve: Curves.elasticInOut,
-                              duration: Duration(milliseconds: 300),
-                            ),
-                            backgroundColor: Colors.transparent,
-                            barrierColor: Colors.transparent,
-                            builder: (context) {
-                              return Stack(
-                                children: [
-                                  Positioned.fill(
-                                    child: ClipRRect(
-                                      child: BackdropFilter(
-                                        filter: ImageFilter.blur(
-                                          sigmaX: 6,
-                                          sigmaY: 6,
-                                        ),
-                                        child: Container(
-                                          color: Colors.transparent,
+                background: ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8).r,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(child: CustomSearchField()),
+                          AppConstants.horizontalSmallSizedBox,
+                          CustomImgIconButton(
+                            path: ImagesPath.menuIcon,
+                            onPressed: () {
+                              showModalBottomSheet(
+                                context: context,
+                                useSafeArea: true,
+                                constraints: BoxConstraints(maxHeight: 400.h),
+                                sheetAnimationStyle: AnimationStyle(
+                                  curve: Curves.elasticInOut,
+                                  duration: Duration(milliseconds: 300),
+                                ),
+                                backgroundColor: Colors.transparent,
+                                barrierColor: Colors.transparent,
+                                builder: (context) {
+                                  return Stack(
+                                    children: [
+                                      Positioned.fill(
+                                        child: ClipRRect(
+                                          child: BackdropFilter(
+                                            filter: ImageFilter.blur(
+                                              sigmaX: 6,
+                                              sigmaY: 6,
+                                            ),
+                                            child: Container(
+                                              color: Colors.transparent,
+                                            ),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  ),
-                                  Container(
-                                    clipBehavior: Clip.hardEdge,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(30),
-                                        topRight: Radius.circular(30),
-                                      ).r,
-                                      color: AppColors.previewTextBgColor,
-                                    ),
-                                    child: ListView.custom(
-                                      shrinkWrap: true,
-                                      padding: EdgeInsets.all(10).r,
-                                      keyboardDismissBehavior:
-                                          ScrollViewKeyboardDismissBehavior
-                                              .manual,
-                                      childrenDelegate: SliverChildListDelegate(
-                                        [const BottomSheetCustomButtonWIcon()],
+                                      Container(
+                                        clipBehavior: Clip.hardEdge,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(30),
+                                            topRight: Radius.circular(30),
+                                          ).r,
+                                          color: AppColors.previewTextBgColor,
+                                        ),
+                                        child: ListView.custom(
+                                          shrinkWrap: true,
+                                          padding: EdgeInsets.all(10).r,
+
+                                          keyboardDismissBehavior:
+                                              ScrollViewKeyboardDismissBehavior
+                                                  .manual,
+                                          childrenDelegate:
+                                              SliverChildListDelegate([
+                                                BottomSheetCustomButtonWIcon(
+                                                  path: ImagesPath.logoutIcon,
+                                                  title: 'Sign Out',
+                                                  colors: [
+                                                    AppColors.buttonBgColor
+                                                        .withAlpha(210),
+                                                    AppColors.secondaryColor
+                                                        .withAlpha(100),
+                                                    AppColors.infoColor
+                                                        .withAlpha(220),
+                                                  ],
+                                                  onPressed: () async {
+                                                    await serviceLocator<
+                                                          SignOutUseCase
+                                                        >()
+                                                        .call();
+                                                    if (context.mounted) {
+                                                      context.go(
+                                                        NavigatorPath.login,
+                                                      );
+                                                    }
+                                                  },
+                                                ),
+                                                BottomSheetCustomButtonWIcon(
+                                                  path: ImagesPath.settingsIcon,
+                                                  title: 'Settings',
+                                                  colors: [
+                                                    AppColors.buttonBgColor
+                                                        .withAlpha(210),
+                                                    AppColors.secondaryColor
+                                                        .withAlpha(100),
+                                                    AppColors.infoColor
+                                                        .withAlpha(220),
+                                                  ],
+                                                  onPressed: () {},
+                                                ),
+                                              ]),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                ],
+                                    ],
+                                  );
+                                },
                               );
                             },
-                          );
-                        },
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
