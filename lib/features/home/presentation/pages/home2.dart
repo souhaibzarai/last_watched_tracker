69b33919e967b93bd3d72d3f24fe6a8cb5a24611
souhaibzarai <souhaibzarai18@gmail.com>
2025-05-26 20:23:44 +0100

    working on bottom sheet

--- lib/features/home/presentation/pages/home2.dart
+++ lib/features/home/presentation/pages/home2.dart
@@ -3,11 +3,14 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:last_watched_tracker/common/widgets/button/bottom_sheet_custom_button_w_icon.dart';
 
 import '../../../../common/widgets/button/custom_img_icon_button.dart';
+import '../../../../common/widgets/image/custom_image_asset.dart';
 import '../../../../common/widgets/scaffold/custom_app_scaffold.dart';
 import '../../../../utils/constants/constants.dart';
 import '../../../../utils/constants/images_path.dart';
+import '../../../../utils/helpers/images/image_helper.dart';
 import '../../../../utils/theme/app_colors.dart';
 import '../cubit/user_info_cubit.dart';
 import '../widgets/custom_search_field.dart';
@@ -48,18 +51,27 @@ class Home2 extends StatelessWidget {
                             constraints: BoxConstraints(maxHeight: 400.h),
                             sheetAnimationStyle: AnimationStyle(
                               curve: Curves.elasticInOut,
-                              duration: Duration(milliseconds: 500),
+                              duration: Duration(milliseconds: 300),
                             ),
                             backgroundColor: Colors.transparent,
+                            barrierColor: Colors.transparent,
                             builder: (context) {
-                              return ClipRRect(
-                                child: BackdropFilter(
-                                  filter: ImageFilter.blur(
-                                    sigmaX: 6,
-                                    sigmaY: 6,
+                              return Stack(
+                                children: [
+                                  Positioned.fill(
+                                    child: ClipRRect(
+                                      child: BackdropFilter(
+                                        filter: ImageFilter.blur(
+                                          sigmaX: 6,
+                                          sigmaY: 6,
+                                        ),
+                                        child: Container(
+                                          color: Colors.transparent,
+                                        ),
+                                      ),
+                                    ),
                                   ),
-                                  child: Container(
-                                    padding: EdgeInsets.all(10).r,
+                                  Container(
                                     clipBehavior: Clip.hardEdge,
                                     decoration: BoxDecoration(
                                       borderRadius: BorderRadius.only(
@@ -68,13 +80,18 @@ class Home2 extends StatelessWidget {
                                       ).r,
                                       color: AppColors.previewTextBgColor,
                                     ),
-                                    child: Column(
-                                      children: [
-                                        Center(child: Text('Hello World')),
-                                      ],
+                                    child: ListView.custom(
+                                      shrinkWrap: true,
+                                      padding: EdgeInsets.all(10).r,
+                                      keyboardDismissBehavior:
+                                          ScrollViewKeyboardDismissBehavior
+                                              .manual,
+                                      childrenDelegate: SliverChildListDelegate(
+                                        [const BottomSheetCustomButtonWIcon()],
+                                      ),
                                     ),
                                   ),
-                                ),
+                                ],
                               );
                             },
                           );
@@ -86,9 +103,18 @@ class Home2 extends StatelessWidget {
               ),
             ),
             SliverAppBar(
-              backgroundColor: AppColors.secondaryTextColor,
-              toolbarHeight: 800,
+              toolbarHeight: 400,
               automaticallyImplyLeading: false,
+              flexibleSpace: ImageHelper.getImage(
+                imgUrl: ImagesPath.splashImage,
+              ),
+            ),
+            SliverAppBar(
+              toolbarHeight: 400,
+              automaticallyImplyLeading: false,
+              flexibleSpace: ImageHelper.getImage(
+                imgUrl: ImagesPath.splashImage,
+              ),
             ),
           ],
         ),
