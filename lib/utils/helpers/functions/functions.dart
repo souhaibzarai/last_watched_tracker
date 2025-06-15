cd1fdbf5323b08f4e14d90d8ca8d64bd5502a2e9
souhaibzarai <souhaibzarai18@gmail.com>
2025-06-15 21:44:31 +0100

    get rid of redundant cubit usage

--- lib/utils/helpers/functions/functions.dart
+++ lib/utils/helpers/functions/functions.dart
@@ -3,16 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../features/media/data/models/archive.dart';
 import '../../../features/media/presentation/cubit/archive_media_cubit.dart';
-import '../../../features/media/presentation/cubit/fetch_medias_cubit.dart';
 
 class FunctionsHelper {
   FunctionsHelper._();
 
   static void toggleArchive(BuildContext context, ArchiveModel archive) async {
     final archiveCubit = context.read<ArchiveMediaCubit>();
-    final fetchMediaCubit = context.read<FetchMediasCubit>();
 
     await archiveCubit.toggleArchive(archive);
-    await fetchMediaCubit.fetchMedias(showLoading: false);
   }
 }
