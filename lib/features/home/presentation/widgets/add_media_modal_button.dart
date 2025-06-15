d647af717b425a4c58975a89303e2708fb603957
souhaibzarai <souhaibzarai18@gmail.com>
2025-06-15 21:44:55 +0100

    get rid of redundant cubit usage

--- lib/features/home/presentation/widgets/add_media_modal_button.dart
+++ lib/features/home/presentation/widgets/add_media_modal_button.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../common/widgets/button/bottom_sheet_custom_button_w_icon.dart';
 import '../../../../utils/constants/images_path.dart';
 import '../../../../utils/helpers/navigator/strings.dart';
 import '../../../../utils/theme/app_colors.dart';
-import '../../../media/presentation/cubit/fetch_medias_cubit.dart';
 
 class AddMediaModalButton extends StatelessWidget {
   const AddMediaModalButton({
@@ -28,12 +26,10 @@ class AddMediaModalButton extends StatelessWidget {
         final isMediaAdded = await context.push(
           NavigatorPath.addMedia,
         );
-
-        if (isMediaAdded == true) {
+        if (isMediaAdded == true)
           context.pop();
-          return await context.read<FetchMediasCubit>().fetchMedias();
-        } else
-          return context.pop();
+        else
+          context.pop();
       },
     );
   }
